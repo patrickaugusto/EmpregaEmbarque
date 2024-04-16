672d8504bce541a220aeff0383bb3c21070645ef
patrick_augusto <68711916+patrickaugusto@users.noreply.github.com>
2024-04-16 13:39:58 -0300

    Add files via upload

---
+++ portfolio/src/pages/Transmissao/index.jsx
@@ -0,0 +1,14 @@
+import Nav from '../../components/Nav'
+import Footer from '../../components/Footer';
+
+function Transmissao() {
+  return (
+    <>
+      <Nav />
+      <h1>PAgina de transmissão</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default Transmissao;
---
+++ portfolio/src/pages/Vagas/index.jsx
@@ -0,0 +1,14 @@
+import Nav from '../../components/Nav'
+import Footer from '../../components/Footer'
+
+function Vagas() {
+  return (
+    <>
+      <Nav />
+      <h1>PAgina de vagas</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default Vagas
