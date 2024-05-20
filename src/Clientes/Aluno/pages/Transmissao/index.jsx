caf741254d5fe071b3b41202232c7a79256c5248
aluno <aluno0@unicap.br>
2024-05-20 08:59:22 -0300

    .

--- src/Clientes/Aluno/pages/Transmissao/index.jsx
+++ src/Clientes/Aluno/pages/Transmissao/index.jsx
@@ -1,11 +1,19 @@
 import Nav from '/src/components/NavLogado/index'
 import Footer from '/src/components/Footer/index';
+import { Container } from '@chakra-ui/react';
 
 function Transmissao() {
   return (
     <>
       <Nav />
-      <h1>PAgina de transmissão</h1>
+      <h1>Pagina de transmissão</h1>
+      <Container>
+        <div>
+          <p>
+            add as coisas
+          </p>
+        </div>
+      </Container>
       <Footer />
     </>
   );
